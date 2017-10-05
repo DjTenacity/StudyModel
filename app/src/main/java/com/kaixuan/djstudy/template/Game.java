2c77dcde485e2c81ef42023359270b6a13da9881
gdjsdk <1757286697@qq.com>
2017-10-05 22:07:01 +0800

    模板模式

---
+++ app/src/main/java/com/kaixuan/djstudy/template/Game.java
@@ -0,0 +1,27 @@
+package com.kaixuan.djstudy.template;
+
+/**
+ * Comment:定义算法结构,还可以提供通用实现
+ *
+ * @author :DJ鼎尔东 / 1757286697@qq.cn
+ * @version : Administrator1.0
+ * @date : 2017/10/5
+ */
+public abstract class Game {
+    abstract void login();
+
+    abstract void startPlay();
+
+    abstract void endPlay();
+
+
+    public final void play() {
+        System.out.println("开始");
+        login();
+        startPlay();
+        endPlay();
+        System.out.println("结束");
+
+    }
+
+}
---
+++ app/src/main/java/com/kaixuan/djstudy/template/LoLGame.java
@@ -0,0 +1,28 @@
+package com.kaixuan.djstudy.template;
+
+/**
+ * Comment:
+ *
+ * @author :DJ鼎尔东 / 1757286697@qq.cn
+ * @version : Administrator1.0
+ * @date : 2017/10/5
+ */
+public class LoLGame extends Game {
+    @Override
+    void login() {
+        System.out.println("开始 login");
+
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("开始游戏");
+
+    }
+
+    @Override
+    void endPlay() {
+        System.out.println("结束游戏");
+
+    }
+}
